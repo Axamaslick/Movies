c587d7bcb401924ee4039a2bd848005cc6c7092b
Lev <kartoxauaua@gmail.com>
2025-04-06 23:15:24 +0700

    Объявление переменных

--- app/src/main/java/com/example/movies/MainActivity.kt
+++ app/src/main/java/com/example/movies/MainActivity.kt
@@ -1,12 +1,26 @@
 package com.example.movies
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import androidx.lifecycle.Observer
+import androidx.activity.viewModels
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var fabAddMovie: FloatingActionButton
+    private lateinit var btnDeleteSelected: Button
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +30,17 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        recyclerView = findViewById(R.id.moviesList)
+        fabAddMovie = findViewById(R.id.fabAddMovie)
+        btnDeleteSelected = findViewById(R.id.btnDeleteSelected)
+
+
+
+        fabAddMovie.setOnClickListener {
+            startActivity(Intent(this, AddActivity::class.java))
+        }
+
+
     }
 }
