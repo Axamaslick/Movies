520ccd8ce2e32f2f89f09586c57bcc6d8d3b285f
Lev <kartoxauaua@gmail.com>
2025-04-07 00:14:30 +0700

    Добавление базы для Movie

---
+++ app/src/main/java/com/example/movies/MovieViewModel.kt
@@ -0,0 +1,29 @@
+package com.example.movies
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class MovieViewModel(private val movieDao: MovieDao) : ViewModel() {
+    val allMovies: LiveData<List<Movie>> = movieDao.getAllMovies()
+
+    fun deleteMovie(movie: Movie) {
+        viewModelScope.launch(Dispatchers.IO) {
+            movieDao.deleteMovie(movie)
+        }
+    }
+
+    fun deleteSelectedMovies(movies: List<Movie>) {
+        viewModelScope.launch(Dispatchers.IO) {
+            movieDao.deleteMovies(movies)
+        }
+    }
+
+    fun insertMovie(movie: Movie) {
+        viewModelScope.launch(Dispatchers.IO) {
+            movieDao.insertMovie(movie)
+        }
+    }
+}
