59753b0b61c2e2580345049f8494072b4361380b
Lev <kartoxauaua@gmail.com>
2025-04-07 00:15:01 +0700

    Добавление Movie

---
+++ app/src/main/java/com/example/movies/Movie.kt
@@ -0,0 +1,13 @@
+package com.example.movies
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "movies")
+data class Movie(
+    @PrimaryKey(autoGenerate = true) val id: Int = 0,
+    val title: String,
+    val year: String,
+    val posterUrl: String, // Field for storing the poster URL
+    var isSelected: Boolean = false
+)
