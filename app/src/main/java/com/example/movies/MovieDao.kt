94cfeb40aac60f5832383f1c91859ae70ebc9006
Lev <kartoxauaua@gmail.com>
2025-04-07 00:15:49 +0700

    Добавление MovieDao

---
+++ app/src/main/java/com/example/movies/MovieDao.kt
@@ -0,0 +1,20 @@
+package com.example.movies
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+
+@Dao
+interface MovieDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertMovie(movie: Movie)
+
+    @Query("SELECT * FROM movies")
+    fun getAllMovies(): LiveData<List<Movie>>
+
+    @Delete
+    suspend fun deleteMovie(movie: Movie)
+
+    @Transaction
+    @Delete
+    suspend fun deleteMovies(movies: List<Movie>)
+}
