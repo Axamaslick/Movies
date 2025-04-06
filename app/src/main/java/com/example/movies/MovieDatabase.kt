4589deff167c4cb77fe3d5acc2b7bac9cd2f6287
Lev <kartoxauaua@gmail.com>
2025-04-07 00:23:59 +0700

    Добавление MovieDatabase

---
+++ app/src/main/java/com/example/movies/MovieDatabase.kt
@@ -0,0 +1,32 @@
+package com.example.movies
+
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import android.content.Context
+import androidx.sqlite.db.SupportSQLiteDatabase
+import androidx.room.migration.Migration
+
+@Database(entities = [Movie::class], version = 2, exportSchema = false)
+abstract class MovieDatabase : RoomDatabase() {
+    abstract fun movieDao(): MovieDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: MovieDatabase? = null
+
+        fun getDatabase(context: Context): MovieDatabase {
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    MovieDatabase::class.java,
+                    "movie_database"
+                )
+                    .addMigrations(MIGRATION_1_2) // Add the migration here!
+                    .build()
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
+}
